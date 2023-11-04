11fdfb497b999079d5231c1a6d4068a1f0911ecc
Lyzev <lyzev.info@gmail.com>
2023-11-04 01:35:00 +0100

    Feature System #7

--- src/main/kotlin/dev/lyzev/schizoid/feature/Feature.kt
+++ src/main/kotlin/dev/lyzev/schizoid/feature/Feature.kt
@@ -15,7 +15,9 @@ import dev.lyzev.api.settings.KeySetting
  * @param key The keybind of the feature.
  * @property category The category of the feature.
  */
-abstract class Feature(val name: String, val desc: String, key: Int, val category: Category) {
+abstract class Feature(
+    val name: String, val desc: String, vararg val aliases: String, key: Int, val category: Category
+) {
 
     // The keybind of the feature.
     val keybind by KeySetting(this::class, "Key", key)
