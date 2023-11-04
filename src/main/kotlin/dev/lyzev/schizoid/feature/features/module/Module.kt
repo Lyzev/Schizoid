26e2362b4a3b9e83af31d04a5029c5edc5b9c4aa
Lyzev <lyzev.info@gmail.com>
2023-11-04 02:07:43 +0100

    Module System #4

--- src/main/kotlin/dev/lyzev/schizoid/feature/features/module/Module.kt
+++ src/main/kotlin/dev/lyzev/schizoid/feature/features/module/Module.kt
@@ -18,8 +18,57 @@ import dev.lyzev.schizoid.feature.Feature
  * @property category The category of the module.
  */
 abstract class Module(name: String, desc: String, vararg aliases: String, key: Int = -1, category: Category) :
-    Feature(name, desc, aliases = aliases, key, category) {
+    Feature(name, desc, aliases = aliases, key, category)
+
+/**
+ * Represents a module that can be run.
+ *
+ * @property name The name of the module.
+ * @property desc The description of the module.
+ * @property aliases The aliases of the module.
+ * @param key The keybind of the module.
+ * @property category The category of the module.
+ */
+abstract class ModuleRunnable(name: String, desc: String, vararg aliases: String, key: Int = -1, category: Category) :
+    Module(name, desc, aliases = aliases, key, category), () -> Unit
+
+/**
+ * Represents a module that can be toggled.
+ *
+ * @property name The name of the module.
+ * @property desc The description of the module.
+ * @property aliases The aliases of the module.
+ * @param key The keybind of the module.
+ * @property category The category of the module.
+ */
+abstract class ModuleToggleable(name: String, desc: String, vararg aliases: String, key: Int = -1, category: Category) :
+    Module(name, desc, aliases = aliases, key, category) {
+
+    // Indicates whether the module is enabled.
+    var isEnabled by BooleanSetting(this::class, "Enabled", false) {
+        if (it) onEnable()
+        else onDisable()
+    }
 
     // Indicates whether the module should be shown in the array list.
     var showInArrayList by BooleanSetting(this::class, "Show In ArrayList", true)
+
+    /**
+     * Toggles the module.
+     *
+     * @see isEnabled
+     */
+    fun toggle() {
+        isEnabled = !isEnabled
+    }
+
+    /**
+     * Called when the module is enabled.
+     */
+    protected open fun onEnable() {}
+
+    /**
+     * Called when the module is disabled.
+     */
+    protected open fun onDisable() {}
 }
