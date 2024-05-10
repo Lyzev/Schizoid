cf09ebc25707f9c71d2779513126f84db2dc0c36
Lyzev <lyzev.info@gmail.com>
2024-05-10 14:59:11 +0200

    improved code readability
    Signed-off-by: Lyzev <lyzev.info@gmail.com>

--- src/main/resources/assets/schizoid/shaders/core/Particle/Particle_CP.glsl
+++ src/main/resources/assets/schizoid/shaders/core/Particle/Particle_CP.glsl
@@ -43,8 +43,8 @@ void main() {
         float speed = min(MAX_SPEED, MIN_SPEED + length(GRAVITY * GRAVITY * dist));
         float phi = atan(dist.y, dist.x);
         phi += (rand(p.position) - .5) * (PI / 4); // -22.5 to 22.5 degrees
-        vec2 dir = vec2(cos(phi), sin(phi)) * speed;
-        p.motion += dir * force * time;
+        vec2 dir = vec2(cos(phi), sin(phi));
+        p.motion += dir * speed * force * time;
     }
     vec2 pre = p.position;
     p.position += p.motion;
