c5e662e23c810a5be4a79515504896e584fa8a8d
Lyzev <lyzev.info@gmail.com>
2024-05-11 19:08:22 +0200

    [skip ci] modified default settings and added a units for some of the settings

--- src/main/kotlin/dev/lyzev/schizoid/feature/features/module/modules/render/ModuleToggleableColorGrading.kt
+++ src/main/kotlin/dev/lyzev/schizoid/feature/features/module/modules/render/ModuleToggleableColorGrading.kt
@@ -22,15 +22,15 @@ import java.awt.Color
 
 object ModuleToggleableColorGrading : ModuleToggleable("Color Grading", "Enables color grading for the world.", category = IFeature.Category.RENDER), EventListener {
 
-    val brightness by slider("Brightness", "The brightness of the color grading effect.", -0.1f, -1f, 1f, 2)
-    val contrast by slider("Contrast", "The contrast of the color grading effect.", .95f, 0f, 2f, 2)
-    val exposure by slider("Exposure", "The exposure of the color grading effect.", -0.45f, -1f, 1f, 2)
-    val saturation by slider("Saturation", "The saturation of the color grading effect.", 1.2f, 0f, 2f, 2)
+    val brightness by slider("Brightness", "The brightness of the color grading effect.", -50, -100, 100, "%%")
+    val contrast by slider("Contrast", "The contrast of the color grading effect.", 55, 0, 200, "%%")
+    val exposure by slider("Exposure", "The exposure of the color grading effect.", -0, -100, 100, "%%")
+    val saturation by slider("Saturation", "The saturation of the color grading effect.", 110, 0, 200, "%%")
     val temperature by slider("Temperature", "The temperature of the color grading effect.", 1000, 1000, 40000, "K")
     val lift by color("Lift", value = Color(0, 0, 0))
-    val gamma by color("Gamma", value = Color(0,2,34))
-    val gain by color("Gain", value = Color(11,0,255))
-    val offset by color("Offset", value = Color(0,0,17))
+    val gamma by color("Gamma", value = Color(0,35,0))
+    val gain by color("Gain", value = Color(0,0,255))
+    val offset by color("Offset", value = Color(0,0,0))
 
     override val shouldHandleEvents: Boolean
         get() = isEnabled && !mc.gameRenderer.isRenderingPanorama
@@ -46,10 +46,10 @@ object ModuleToggleableColorGrading : ModuleToggleable("Color Grading", "Enables
             RenderSystem.activeTexture(GL_TEXTURE0)
             mc.framebuffer.beginRead()
             ShaderColorGrading["Tex0"] = 0
-            ShaderColorGrading["Brightness"] = brightness
-            ShaderColorGrading["Contrast"] = contrast
-            ShaderColorGrading["Exposure"] = exposure
-            ShaderColorGrading["Saturation"] = saturation
+            ShaderColorGrading["Brightness"] = brightness / 100f
+            ShaderColorGrading["Contrast"] = contrast / 100f
+            ShaderColorGrading["Exposure"] = exposure / 100f
+            ShaderColorGrading["Saturation"] = saturation / 100f
             ShaderColorGrading["Temperature"] = temperature.toFloat()
             ShaderColorGrading["Lift"] = lift
             ShaderColorGrading["Gamma"] = gamma
